49e2f3b5048667e59c23fa12b12837a913d6a1ea
proxmetei <aleksei.korchagin200@mail.ru>
2021-10-20 17:04:09 +0300

    Initial Commit

---
+++ iosLab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosLab2
+//
+//  Created by Алексей on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
